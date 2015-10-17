9ed7037862f51d30ba1c7cc6a149220493528015
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2015-10-18 00:05:24 +0200

    add pre-trained two-stream cnn

--- RGBCNNFeature.py
+++ RGBCNNFeature.py
@@ -10,6 +10,16 @@ from caffeCNN import caffe_init, caffe_predict
 
 def RGBCNNFeature(vid_name, use_gpu, NUM_HEIGHT, NUM_WIDTH, model_def_file, model_file, gpu_id):
 
+    # Initialize caffe net
+    net = caffe_init(use_gpu,model_def_file,model_file,gpu_id)
+    N, C, H, W = net.blobs[net.inputs[0]].data.shape
+    N, d1, d2, d3 = net.blobs[net.outputs[0]].data.shape
+
+    if NUM_HEIGHT != H:
+        raise Exception, 'HEIGHT is not euqual to pre-trained network config!'
+    if NUM_WIDTH != W:
+        raise Exception, 'WIDTH is not euqual to pre-trained network config'
+
     # Input video
     vidCap = cv2.VideoCapture(vid_name)
     if(!vidCap.isOpened()):  # check if we succeeded
@@ -23,10 +33,10 @@ def RGBCNNFeature(vid_name, use_gpu, NUM_HEIGHT, NUM_WIDTH, model_def_file, mode
         duration = numFrame
     #
     d = scipy.io.loadmat('VGG_mean.mat')
-    IMAGE_MEAN = d['image_mean'];
-    IMAGE_MEAN = imresize(IMAGE_MEAN, (NUM_HEIGHT, NUM_WIDTH));
+    IMAGE_MEAN = d['image_mean']
+    IMAGE_MEAN = imresize(IMAGE_MEAN, (NUM_HEIGHT, NUM_WIDTH))å
 
-    video = np.zeros((NUM_HEIGHT, NUM_WIDTH, 3, duration), dtype=np.float32)
+    video = np.zeros((duration, 3, NUM_HEIGHT, NUM_WIDTH), dtype=np.float32)
     for i in range(0, duration):
         flag, frame = vidCap.read()
         if flag:
@@ -34,82 +44,37 @@ def RGBCNNFeature(vid_name, use_gpu, NUM_HEIGHT, NUM_WIDTH, model_def_file, mode
             if len(frame.shape) == 2:
                 frame = np.tile(frame[:,:,np.newaxis], (1,1,3))
 
-            # OpenCV BGR -> RGB (caffe uses BGR)
+            # OpenCV BGR -> RGB ?? (caffe uses BGR)
             # frame = frame[:,:,(2,1,0)]
             # resize
             frame = imresize(frame, (NUM_HEIGHT, NUM_WIDTH), 'bilinear')
             # mean subtraction
             frame = frame - IMAGE_MEAN
-            # get channel in correct dimension
-            # frame = np.transpose(frame, (1,0,2))
-            video[:,:,:,i] = frame
+            # get channel in correct dimension (H,W,C) -> (C,H,W)
+            frame = np.transpose(frame, (2,0,1))
+            #
+            video[i,:,:,:] = frame
         else:
             # The next frame is not ready, so we try to read it again
             print 'frame is not ready'
 
-    # Initialize caffe net
-    net = caffe_init(use_gpu,model_def_file,model_file,gpu_id)
-    N, C, H, W = net.blobs[net.inputs[0]].data.shape
-    N, C, F,  = net.blobs[net.outputs[0]].data.shape
-
     # Computing convoltuional maps
-    # BGR -> RGB
-    # video = video[:,:,[3,2,1],:]
-    # (H,W) -> (W,H)
-    video = np.transpose(video, (1,0,2,3))
-
+    # Keep in mind that width is the fastest dimension and channels are BGR (in Matlab)
+    # however, Matlab (W,H,C,N) -> Python (N,C,H,W)
     batch_size = N # batch_size = 40
-    FCNNFeature = np.zeros((, , , duration), dtype=np.float32)
-    batch_images = np.zeros((NUM_WIDTH, NUM_HEIGHT, 3, batch_size), dtype=np.float32)
+    batch_images = np.zeros((batch_size, 3, NUM_HEIGHT, NUM_WIDTH), dtype=np.float32)
+
+    FCNNFeature = np.zeros((duration, d1, d2, d3), dtype=np.float32)
     for j in range(0, duration, batch_size):
         batch_range = range(j, min(j+batch_size, duration))
-        batch_images[:,:,:,0:len(batch_range)] = video[:,:,:,batch_range]
+        batch_images[0:len(batch_range),:,:,:] = video[batch_range,:,:,:]
 
+        # Predict CNN feature
         feature = caffe_predict(batch_images, net)
-        feature = np.transpose(feature, (1,0,2,3))
+        FCNNFeature[batch_range,:,:,:] = feature[0:len(batch_range),:,:,:]
 
-        FCNNFeature[:,:,:,batch_range] = feature[:,:,:,0:len(batch_range)]
+    # tranpose to matlab format (N,C,H,W) -> (H,W,C,N) not (W,H,C,N)
+    FCNNFeature = np.transpose(FCNNFeature, (2,3,1,0))
 
-
-# Keep in mind that width is the fastest dimension and channels are BGR
-
-% Computing convoltuional maps
-d = load('VGG_mean');
-IMAGE_MEAN = d.image_mean;
-IMAGE_MEAN = imresize(IMAGE_MEAN,[NUM_HEIGHT,NUM_WIDTH]);
-video = video(:,:,[3,2,1],:);
-video = bsxfun(@minus,video,IMAGE_MEAN);
-video = permute(video,[2,1,3,4]);
-
-batch_size = 40;
-num_images = size(video,4);
-num_batches = ceil(num_images/batch_size);
-FCNNFeature = [];
-
-images = zeros(NUM_WIDTH, NUM_HEIGHT, 3, batch_size, 'single');
-for bb = 1 : num_batches
-    range = 1 + batch_size*(bb-1): min(num_images,batch_size*bb);
-    tmp = video(:,:,:,range);
-    images(:,:,:,1:size(tmp,4)) = tmp;
+    return FCNNFeature
     
-    feature = net.forward({images}); %feature = caffe('forward',{images});
-    feature = permute(feature{1},[2,1,3,4]);
-    if isempty(FCNNFeature)
-        FCNNFeature = zeros(size(feature,1), size(feature,2), size(feature,3), num_images, 'single');
-    end
-    FCNNFeature(:,:,:,range) = feature(:,:,:,mod(range-1,batch_size)+1);
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- script_demo.py
+++ script_demo.py
@@ -2,3 +2,34 @@ import sys
 import os.path
 import argparse
 
+import numpy as np
+import scipy.io
+
+sizes = np.array([[8,8], [11.4286,11.4286], [16,16], [22.8571,24], [32,34.2587]])
+sizes_vid = np.array([[480,640], [340,454], [240,320], [170,227], [120,160]])
+
+def main(options):
+	import pdb; pdb.set_trace()
+	
+	# Spatial TDD
+	print 'Extract spatial TDD...'
+
+	scale = 3
+	layer = 'conv5'
+	gpu_id = 1
+
+	model_def_file = 'models/rgb_'+layer+'_scale'+str(scale)+'.prototxt'
+	model_file = 'spatial.caffemodel'
+
+	feature_conv = RGBCNNFeature(options['videofile'], 1, sizes_vid[scale,1], sizes_vid[scale,2], model_def_file, model_file, gpu_id)
+
+	scipy.io.savemat(os.path.join('./', 'FCNNFeature_py.mat'), mdict = {'FCNNFeature': feature_conv})
+
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser()
+  
+  #
+  parser.add_argument('-v', '--videofile', dest='videofile', default='test.avi', help='video: filename')
+
