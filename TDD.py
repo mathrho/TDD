64706de4ab083ce19d12aaa8139187bb1835c65b
mathrho <author@corpmail.com>
2015-10-21 20:33:40 +0200

    add TDD py code

--- TDD.py
+++ TDD.py
@@ -3,35 +3,35 @@ import os.path
 
 import numpy as np
 
-def TDD(inf,tra,cnn_feature,scale_x,scale_y,num_cell):
+def TDD(inf,traj,cnn_feature,scale_x,scale_y,num_cell):
 
 	if inf is not None:
 		ind = np.where(inf[6,:] == 1)[0]
 		inf = inf[:,ind]
-		tra = tra[:,ind]
+		traj = traj[:,ind]
 
 	if inf is not None:
 		NUM_DIM = cnn_feature.shape[2]
 		NUM_DES = inf.shape[1]
-		TRA_LEN = tra.shape[0]/2
+		TRAJ_LEN = traj.shape[0]/2
 
-		num_fea = TRA_LEN / num_cell
+		num_fea = TRAJ_LEN / num_cell
 
-		pos = np.reshape(tra, (2,-1), order='F') - 1
+		pos = np.reshape(traj, (2,-1), order='F') - 1
 		pos = np.divide(pos, [[scale_x],[scale_y]]) + 1
 		#pos = np.around(pos) round half-number round even number, 0.5->0
 		pos = np.floor(np.abs(pos) + 0.5) * np.sign(pos) 
 		pos = np.fmax(pos, [[1],[1]])
 		pos = np.fmin(pos, [[cnn_feature.shape[1]],[cnn_feature.shape[0]]])
-		pos = np.reshape(pos, (TRA_LEN*2,-1))
+		pos = np.reshape(pos, (TRAJ_LEN*2,-1))
 
 		cnn_feature = np.transpose(cnn_feature, (0,1,3,2))
-		offset = np.arange(TRA_LEN-1,-1,-1)
+		offset = np.arange(TRAJ_LEN-1,-1,-1)
 		size_mat = cnn_feature.shape[0:3]
 		cnn_feature = np.reshape(cnn_feature, (-1,NUM_DIM))
 
-		cur_x = np.ravel(pos[range(0,TRA_LEN*2,2),:], order='F') - 1
-		cur_y = np.ravel(pos[range(1,TRA_LEN*2,2),:], order='F') - 1
+		cur_x = np.ravel(pos[range(0,TRAJ_LEN*2,2),:], order='F') - 1
+		cur_y = np.ravel(pos[range(1,TRAJ_LEN*2,2),:], order='F') - 1
 		cur_t= np.ravel(np.subtract(inf[0,:], np.transpose(offset[np.newaxis,:])), order='F') - 1
 		cur = np.array([cur_y, cur_x, cur_t], np.int64)
 
