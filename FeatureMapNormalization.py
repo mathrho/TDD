95d295efcb74e22ec2f4eefac321283f73242ec0
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2015-10-21 03:23:24 +0200

    add normalization code

--- FeatureMapNormalization.py
+++ FeatureMapNormalization.py
@@ -13,12 +13,12 @@ def FeatureMapNormalization(cnn_feature):
 	cnn_feature1 = np.reshape(cnn_feature1, (r*c*t,f), order='F')
 	
 	max_cnn_feature1 = np.amax(cnn_feature1, axis=0) + np.finfo(np.float32).eps
-	cnn_feature1 = cnn_feature1 - np.tile(max_cnn_feature1, (r*c*t,1))
+	cnn_feature1 = np.divide(cnn_feature1, max_cnn_feature1) # np.tile(max_cnn_feature1, (r*c*t,1))
 
 	cnn_feature1 = np.reshape(cnn_feature1, (r,c,t,f), order='F')
 	cnn_feature1 = np.transpose(cnn_feature1, (0,1,3,2))
 
 	max_cnn_feature = np.amax(cnn_feature, axis=2) + np.finfo(np.float32).eps
-	cnn_feature2 = cnn_feature - np.tile(max_cnn_feature[:,:,np.newaxis,:], (1,1,f,1))
+	cnn_feature2 = np.divide(cnn_feature, max_cnn_feature) # np.tile(max_cnn_feature[:,:,np.newaxis,:], (1,1,f,1))
 
 	return cnn_feature1, cnn_feature2
