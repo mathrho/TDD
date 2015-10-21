8448c3bfb618f19c68a6d30e5bf49f1376e7e9f2
mathrho <author@corpmail.com>
2015-10-21 16:49:32 +0200

    add TDD py code

--- TDD.py
+++ TDD.py
@@ -1,40 +1,47 @@
-function [feature] = TDD(inf,tra,cnn_feature,scale_x,scale_y,num_cell)
+import sys
+import os.path
 
-if ~isempty(inf)
-	ind = inf(7,:)==1;
-	inf = inf(:,ind);
-	tra = tra(:,ind);
-end
+import numpy as np
 
+def TDD(inf,tra,cnn_feature,scale_x,scale_y,num_cell):
 
-if ~isempty(inf)
-	NUM_DIM = size(cnn_feature,3);
-	NUM_DES = size(inf,2);
-	TRA_LEN = size(tra,1)/2;
+	if inf is not None:
+		ind = np.where(inf[6,:] == 1)[0]
+		inf = inf[:,ind]
+		tra = tra[:,ind]
 
-	num_fea = TRA_LEN / num_cell;
+	if inf is not None:
+		NUM_DIM = cnn_feature.shape[2]
+		NUM_DES = inf.shape[1]
+		TRA_LEN = tra.shape[0]/2
 
-	pos = reshape(tra,2,[])-1;
-	pos = round(bsxfun(@rdivide,pos,[scale_x;scale_y]) + 1);
-	pos = bsxfun(@max,pos,[1;1]);
-	pos = bsxfun(@min,pos,[size(cnn_feature,2);size(cnn_feature,1)]);
-	pos = reshape(pos,TRA_LEN*2,[]);
+		num_fea = TRA_LEN / num_cell
 
-	cnn_feature = permute(cnn_feature,[1,2,4,3]);
-	offset = [TRA_LEN-1:-1:0];
-	size_mat = [size(cnn_feature,1),size(cnn_feature,2),size(cnn_feature,3)];
-	cnn_feature = reshape(cnn_feature,[],NUM_DIM);
+		pos = np.reshape(tra, (2,-1), order='F') - 1
+		pos = np.divide(pos, [[scale_x],[scale_y]]) + 1
+		#pos = np.around(pos) round half-number round even number, 0.5->0
+		pos = np.floor(np.abs(pos) + 0.5) * np.sign(pos) 
+		pos = np.fmax(pos, [[1],[1]])
+		pos = np.fmin(pos, [[cnn_feature.shape[1]],[cnn_feature.shape[0]]])
+		pos = np.reshape(pos, (TRA_LEN*2,-1))
 
-	cur_x = pos(1:2:end,:);
-	cur_y = pos(2:2:end,:);
-	cur_t = bsxfun(@minus,inf(1,:),offset');
+		cnn_feature = np.transpose(cnn_feature, (0,1,3,2))
+		offset = range(TRA_LEN-1,-1,-1)
+		size_mat = [cnn_feature.shape[0],cnn_feature.shape[1],cnn_feature.shape[2]]
+		cnn_feature = np.reshape(cnn_feature, (-1,NUM_DIM))
 
-	tmp = cnn_feature(sub2ind(size_mat,cur_y,cur_x,cur_t),:)';
-	tmp = reshape(tmp,NUM_DIM,num_fea,[]);
-	feature = reshape(sum(tmp,2),[],NUM_DES);
-else
-	feature = [];
-end
+		cur_x = pos[range(0,TRA_LEN*2,2),:]
+		cur_y = pos[range(1,TRA_LEN*2,2),:]
+		cur_t= np.subtract(inf[0,:], np.transpose(offset[np.newaxis,:]))
 
+		tmp = cnn_feature[np.ravel_multi_index([cur_y,cur_x,cur_t],size_mat), :]
+		tmp = np.transpose(tmp)
+		tmp = np.reshape(tmp, (NUM_DIM,num_fea,-1))
+		feature = np.reshape(np.sum(tmp,axis=1), (-1,NUM_DES))
 
-end
+	else:
+
+		feature = []
+
+	return feature
+
--- script_demo.py
+++ script_demo.py
@@ -9,6 +9,7 @@ import scipy.io
 from RGBCNNFeature import RGBCNNFeature
 from FlowCNNFeature import FlowCNNFeature
 from FeatureMapNormalization import FeatureMapNormalization
+from TDD import TDD
 
 sizes = np.array([[8,8], [11.4286,11.4286], [16,16], [22.8571,24], [32,34.2587]])
 sizes_vid = np.array([[480,640], [340,454], [240,320], [170,227], [120,160]])
@@ -44,7 +45,10 @@ def main(options):
 	feature_conv = RGBCNNFeature(options['videofile'], 1, sizes_vid[scale-1,0], sizes_vid[scale-1,1], model_def_file, model_file, gpu_id)
 	feature_conv_normalize_1, feature_conv_normalize_2 = FeatureMapNormalization(feature_conv);
 
-	scipy.io.savemat(os.path.join('./', 'rgbCNNFeature_py.mat'), mdict = {'feature_conv_normalize_1': feature_conv_normalize_1, 'feature_conv_normalize_2': feature_conv_normalize_2})
+	tdd_feature_spatial_1 = TDD(info, tra, feature_conv_normalize_1, sizes[scale-1,0], sizes[scale-1,1], 1)
+	tdd_feature_spatial_2 = TDD(info, tra, feature_conv_normalize_2, sizes[scale-1,0], sizes[scale-1,2], 1)
+
+	scipy.io.savemat(os.path.join('./', 'rgbCNNFeature_py.mat'), mdict = {'tdd_feature_spatial_1': tdd_feature_spatial_1, 'tdd_feature_spatial_2': tdd_feature_spatial_2})
 
 
 	# Temporal TDD
@@ -62,7 +66,10 @@ def main(options):
 	feature_conv = FlowCNNFeature('test/', 1, sizes_vid[scale-1,0], sizes_vid[scale-1,1], model_def_file, model_file, gpu_id)
 	feature_conv_normalize_1, feature_conv_normalize_2 = FeatureMapNormalization(feature_conv);
 
-	scipy.io.savemat(os.path.join('./', 'flowCNNFeature_py.mat'), mdict = {'feature_conv_normalize_1': feature_conv_normalize_1, 'feature_conv_normalize_2': feature_conv_normalize_2})
+	tdd_feature_temporal_1 = TDD(info, tra, feature_conv_normalize_1, sizes[scale-1,0], sizes[scale-1,1], 1)
+	tdd_feature_temporal_2 = TDD(info, tra, feature_conv_normalize_2, sizes[scale-1,0], sizes[scale-1,2], 1)
+
+	scipy.io.savemat(os.path.join('./', 'flowCNNFeature_py.mat'), mdict = {'tdd_feature_temporal_1': tdd_feature_temporal_1, 'tdd_feature_temporal_2': tdd_feature_temporal_2})
 
 
 if __name__ == "__main__":
