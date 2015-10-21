ee0816ca2ff8ad972beafa31a91dcadc81721037
mathrho <author@corpmail.com>
2015-10-21 19:45:10 +0200

    add TDD py code

---
+++ IDT_feature.py
@@ -0,0 +1,50 @@
+#
+# Handles a improved trajectory feature point
+# Check http://lear.inrialpes.fr/people/wang/improved_trajectories
+#
+
+import numpy as np
+import os
+INFO_DIM=10
+TRAJ_DIM=30
+TRAJ_SHAPE_DIM=30
+HOG_DIM=96
+HOF_DIM=108
+MBHX_DIM=96
+MBHY_DIM=96
+ALL_DIM=INFO_DIM+TRAJ_DIM+TRAJ_SHAPE_DIM+HOG_DIM+HOF_DIM+MBHX_DIM+MBHY_DIM
+
+#Represents a set of IDTF feature (columns as points)
+class IDTFeature(object):
+    def __init__(self, data):
+
+        data = np.reshape(data, (ALL_DIM, -1), order='F')
+
+        if data.size:
+
+            traj_start = 10
+            traj_shape_start = traj_start + TRAJ_SHAPE_DIM
+            hog_start = traj_shape_start + TRAJ_DIM
+            hof_start = hog_start + HOG_DIM
+            mbhx_start = hof_start + HOF_DIM
+            mbhy_start = mbhx_start + MBHX_DIM
+            mbhy_end = mbhy_start + MBHY_DIM
+
+            self.info = data[0:9,:]
+            self.traj = data[traj_start:traj_shape_start,:]
+            self.traj_shape = data[traj_shape_start:hog_start,:]
+            self.hog = data[hog_start:hof_start,:]
+            self.hof = data[hof_start:mbhx_start,:]
+            self.mbhx = data[mbhx_start:mbhy_start,:]
+            self.mbhy = data[mbhy_start:mbhy_end,:]
+
+
+# Parses a video's IDTF (binary) file and returns IDTF feature
+def read_IDTF_file(vid_feature):
+
+    feature = []
+    data = np.fromfile(vid_feature, dtype=np.float32)
+    feature = IDTFeature(data)
+
+    return feature
+
--- script_demo.py
+++ script_demo.py
@@ -10,6 +10,7 @@ from RGBCNNFeature import RGBCNNFeature
 from FlowCNNFeature import FlowCNNFeature
 from FeatureMapNormalization import FeatureMapNormalization
 from TDD import TDD
+import IDT_feature
 
 sizes = np.array([[8,8], [11.4286,11.4286], [16,16], [22.8571,24], [32,34.2587]])
 sizes_vid = np.array([[480,640], [340,454], [240,320], [170,227], [120,160]])
@@ -28,9 +29,9 @@ def main(options):
 	#system(['./denseFlow_gpu -d 1 -f ',vid_name,' -x test/flow_x -y test/flow_y -b 20 -t 1 -d 3']);
 
 	# Import improved trajectories
-	#IDT = import_idt('test.bin',15);
-	#info = IDT.info;
-	#tra = IDT.tra;
+	IDT = IDT_feature.read_IDTF_file('test.bin')
+	info = IDT.info
+	traj = IDT.traj
 
 	# Spatial TDD
 	print 'Extract spatial TDD...'
@@ -43,10 +44,16 @@ def main(options):
 	model_file = 'spatial.caffemodel'
 
 	feature_conv = RGBCNNFeature(options['videofile'], 1, sizes_vid[scale-1,0], sizes_vid[scale-1,1], model_def_file, model_file, gpu_id)
+	
+	if np.amax(info[0,:]) > feature_conv.shape[3]:
+    	ind =  np.where(info[0,:] <= feature_conv.shape[3])[0]
+    	info = info[:,ind]
+    	traj = traj[:,ind]
+
 	feature_conv_normalize_1, feature_conv_normalize_2 = FeatureMapNormalization(feature_conv);
 
-	tdd_feature_spatial_1 = TDD(info, tra, feature_conv_normalize_1, sizes[scale-1,0], sizes[scale-1,1], 1)
-	tdd_feature_spatial_2 = TDD(info, tra, feature_conv_normalize_2, sizes[scale-1,0], sizes[scale-1,2], 1)
+	tdd_feature_spatial_1 = TDD(info, traj, feature_conv_normalize_1, sizes[scale-1,0], sizes[scale-1,1], 1)
+	tdd_feature_spatial_2 = TDD(info, traj, feature_conv_normalize_2, sizes[scale-1,0], sizes[scale-1,2], 1)
 
 	scipy.io.savemat(os.path.join('./', 'rgbCNNFeature_py.mat'), mdict = {'tdd_feature_spatial_1': tdd_feature_spatial_1, 'tdd_feature_spatial_2': tdd_feature_spatial_2})
 
@@ -64,10 +71,16 @@ def main(options):
 	model_file = 'temporal.caffemodel'
 
 	feature_conv = FlowCNNFeature('test/', 1, sizes_vid[scale-1,0], sizes_vid[scale-1,1], model_def_file, model_file, gpu_id)
+	
+	if np.amax(info[0,:]) > feature_conv.shape[3]:
+    	ind =  np.where(info[0,:] <= feature_conv.shape[3])[0]
+    	info = info[:,ind]
+    	traj = traj[:,ind]
+
 	feature_conv_normalize_1, feature_conv_normalize_2 = FeatureMapNormalization(feature_conv);
 
-	tdd_feature_temporal_1 = TDD(info, tra, feature_conv_normalize_1, sizes[scale-1,0], sizes[scale-1,1], 1)
-	tdd_feature_temporal_2 = TDD(info, tra, feature_conv_normalize_2, sizes[scale-1,0], sizes[scale-1,2], 1)
+	tdd_feature_temporal_1 = TDD(info, traj, feature_conv_normalize_1, sizes[scale-1,0], sizes[scale-1,1], 1)
+	tdd_feature_temporal_2 = TDD(info, traj, feature_conv_normalize_2, sizes[scale-1,0], sizes[scale-1,2], 1)
 
 	scipy.io.savemat(os.path.join('./', 'flowCNNFeature_py.mat'), mdict = {'tdd_feature_temporal_1': tdd_feature_temporal_1, 'tdd_feature_temporal_2': tdd_feature_temporal_2})
 
